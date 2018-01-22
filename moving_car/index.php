94adb7dd83897740669bbfdb1cd12062712038b7
HiranyaJathan <hiranya.jathan@quanical.com>
2018-01-22 18:00:58 +0530

    moving_car animation

---
+++ moving_car/index.php
@@ -0,0 +1,42 @@
+ <!doctype html>
+<html lang="en-US">
+<head>
+	<title>Moving Car</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+	<meta name="viewport" content="width=device-width,initial-scale=1" >
+	<?php include '_partial/head.php' ?>
+</head>
+<body>
+	<header>
+		<?php include '_partial/header.php' ?>
+	</header>
+    
+    
+	<div class="clearfix"></div>
+    
+	<section id="section-home1" class="container-fluid p-lr-none bg-000">		
+		<div class="container p-lr-none">
+            <div class = "col-xs-12 col-sm-12 col-md-12 road-bg p-lr-none">
+                <div class="car-left">
+                    <img src="img/car.png" class="img-responsive" width="300">
+                </div>                        
+            </div>
+            
+            <div class = "col-xs-12 col-sm-12 col-md-12 road-bg border-top p-lr-none">
+                <div class="car-right">
+                    <img src="img/car.png" class="img-responsive" width="300">
+                </div>
+            </div>
+        </div>
+	</section>
+    
+	<div class="clearfix"></div>
+    
+    
+	<footer>
+		<?php include '_partial/footer.php' ?>
+	</footer>	
+	<?php include '_partial/footer-scripts.php' ?>
+</body>
+</html>
